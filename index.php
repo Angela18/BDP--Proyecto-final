2e5e68eb2b8e4ba010fa9a50f749e2c61d56f6f8
Angela Aquise Santos <Angela18@users.noreply.github.com>
2018-07-09 22:59:50 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+date_default_timezone_set('America/Lima');
+#EL INDEX: En él mostraremos la salida de las vistas al usuario y también a traves de él enviaremos las distintas acciones que el usuario envíe al controlador.
+
+#require() establece que el código del archivo invocado es requerido, es decir, obligatorio para el funcionamiento del programa. Por ello, si el archivo especificado en la función require() no se encuentra saltará un error “PHP Fatal error” y el programa PHP se detendrá.
+
+#La versión require_once() funcionan de la misma forma que sus respectivo, salvo que, al utilizar la versión _once, se impide la carga de un mismo archivo más de una vez.
+
+#Si requerimos el mismo código más de una vez corremos el riesgo de redeclaraciones de variables, funciones o clases.
+
+// controlladores
+require_once "controllers/EnlacesController.php";
+require_once "controllers/UsuariosController.php";
+require_once "controllers/RolesController.php";
+require_once "controllers/VentasController.php";
+require_once "controllers/ProductosController.php";
+require_once "controllers/ClientesController.php";
+
+
+// modelos
+require_once "models/Enlace.php";
+require_once "models/Usuario.php";
+require_once "models/Rol.php";
+require_once "models/Producto.php";
+require_once "models/Venta.php";
+require_once "models/Cliente.php";
+//configuracion para la rutaa....
+$mvc = new EnlacesController();
+//templates...
+$mvc->plantilla();
